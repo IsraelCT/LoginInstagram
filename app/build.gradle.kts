04173175ecfa7023e0e69eaaf322ebe000d861bc
Tornado <isracobtor@gmail.com>
2025-04-27 15:06:34 -0600

    Creación de SocialLogin ,Dividir ,mejora del email y Password,LoginButton

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.compose.material:material-icons-extended:1.6.3")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
